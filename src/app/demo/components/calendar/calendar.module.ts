f7ba74e2338378d184778bfbffbe6a1cbc954d4e
YASMINEATTYAOUI <yassmine.attyaoui@fsb.ucar.tn>
2024-05-08 20:59:11 +0100

    calendar

--- src/app/demo/components/calendar/calendar.module.ts
+++ src/app/demo/components/calendar/calendar.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
 import { CalendarRoutingModule } from './calendar-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { DialogModule } from 'primeng/dialog';
+import { DiscountRouterModule } from '../pages/discount/discount-router.module';
 
 @NgModule({
   declarations: [CalendarComponent],
   imports: [
     CommonModule,
-    CalendarRoutingModule
+    CalendarRoutingModule,
+    BrowserModule,
+    RouterOutlet,
+    DiscountRouterModule,
+    FullCalendarModule,
+    DialogModule
   ]
 })
 export class CalendarModule { }
